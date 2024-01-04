181e4233d2f2fd32875cbe81fbe716d934aed74d
ADAM <proselflearner@gmail.com>
2024-01-04 04:33:59 +0300

    Add SmallChallenge.java

---
+++ 0x01-intellij_basics/SmallChallenge.java
@@ -0,0 +1,17 @@
+/**This is just a small challenge*/
+
+public class SmallChallenge {
+    public static void main(String[] args) {
+        // Condition is always true.
+        int newValue = 50;
+        if(newValue == 50){
+            System.out.println("This is true.");
+
+            boolean isCar = false;
+            // condition is always false
+            if (!isCar) {
+                System.out.println("This is not supposed to happen");
+            }
+        }
+    }
+}
