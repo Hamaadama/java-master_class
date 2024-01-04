1f3b690eb5ae98ee27bc9af3ec5fa6d289a6226d
ADAM <proselflearner@gmail.com>
2024-01-04 04:15:23 +0300

    Add FourthClass.java

---
+++ 0x01-intellij_basics/FourthClass.java
@@ -0,0 +1,13 @@
+/**
+ * This file is on logical or operator.
+ */
+
+public class FourthClass {
+    public static void main(String[] args) {
+        int topScore = 100;
+        int secondTopScore = 81;
+        if((topScore > 90) || (secondTopScore <= 90)){
+            System.out.println("Either or both of the conditions are true.");
+        }
+    }
+}
